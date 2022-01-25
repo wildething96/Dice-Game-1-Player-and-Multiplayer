0052c2050eb45a352d6afe89578c409055c653ed
wildething96 <bwilde96@yahoo.co.uk>
2022-01-25 04:25:11 +0000

    fixed end game bug

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ const advancePlayer = () => {
 const restartGame = () => {
   totalScore = 0;
   header.innerHTML = "Current Score";
-  currentScore.innerHTML = totalScore
+  currentScore.innerHTML = totalScore;
   highestScore = 0;
   highestScore = "";
   for (let i = 0; i < players; i++) {
@@ -125,67 +125,69 @@ playerBox[0].style.boxShadow = "0 0 10px 2px yellow";
 playerBox[0].style.opacity = "1.0";
 
 cube.addEventListener("click", (e) => {
-  /* ANIMATION */
+  if (!gameEnd) {
+    /* ANIMATION */
 
-  let randomAngle = Math.floor(Math.random() * 6) + 1 + firstGo;
-  if (randomAngle === 7) {
-    randomAngle -= 1;
-  }
+    let randomAngle = Math.floor(Math.random() * 6) + 1 + firstGo;
+    if (randomAngle === 7) {
+      randomAngle -= 1;
+    }
 
-  console.log(randomAngle);
+    console.log(randomAngle);
 
-  if (randomAngle === 1) {
-    setTimeout(() => {
-      for (let i = 0; i < 6; i++) {
-        sides[i].style.transition = "1s ease";
-        sides[i].style.background =
-          "rgba(0, 0, 0, 0) linear-gradient(to right, rgb(235, 51, 73), rgb(244, 92, 67)) repeat scroll 0% 0%";
-      }
-      gameover.style.visibility = "visible";
-      header.innerHTML = "Game Over";
-      highestScore < totalScore
-        ? ((highestScore = totalScore), (highestScorer = currentPlayer))
-        : 0;
-      totalScore = 0;
-    }, 900);
-  }
-
-  if (out === false) {
-    firstGo = 0;
-    cube.style.animation = "animate 1s linear";
-
-    cube.style.transform =
-      "rotateX(" +
-      angleArray[randomAngle][0] +
-      "deg) rotateY(" +
-      angleArray[randomAngle][1] +
-      "deg) rotateZ(" +
-      angleArray[randomAngle][2] +
-      "deg)";
-    cube.style.transition = "1s linear";
-
-    if (randomAngle !== 1) {
-      cube.addEventListener("animationend", (e) => {
-        cube.style.animation = "";
-      });
+    if (randomAngle === 1) {
       setTimeout(() => {
-        addScore(randomAngle), (currentScore.innerHTML = totalScore);
-      }, 1100);
+        for (let i = 0; i < 6; i++) {
+          sides[i].style.transition = "1s ease";
+          sides[i].style.background =
+            "rgba(0, 0, 0, 0) linear-gradient(to right, rgb(235, 51, 73), rgb(244, 92, 67)) repeat scroll 0% 0%";
+        }
+        gameover.style.visibility = "visible";
+        header.innerHTML = "Game Over";
+        highestScore < totalScore
+          ? ((highestScore = totalScore), (highestScorer = currentPlayer))
+          : 0;
+        totalScore = 0;
+      }, 900);
     }
-  } else {
-    out = false;
-    header.innerHTML = "Current Score";
-    gameover.style.visibility = "hidden";
-    advancePlayer();
-    for (let i = 0; i < 6; i++) {
-      sides[i].style.backgroundColor = "#000000";
-      sides[i].style.backgroundImage =
-        "linear-gradient(147deg, #000000 0%, #2c3e50 74%)";
-    }
-  }
 
-  if (randomAngle === 1) {
-    out = true;
-    firstGo = 1;
+    if (out === false) {
+      firstGo = 0;
+      cube.style.animation = "animate 1s linear";
+
+      cube.style.transform =
+        "rotateX(" +
+        angleArray[randomAngle][0] +
+        "deg) rotateY(" +
+        angleArray[randomAngle][1] +
+        "deg) rotateZ(" +
+        angleArray[randomAngle][2] +
+        "deg)";
+      cube.style.transition = "1s linear";
+
+      if (randomAngle !== 1) {
+        cube.addEventListener("animationend", (e) => {
+          cube.style.animation = "";
+        });
+        setTimeout(() => {
+          addScore(randomAngle), (currentScore.innerHTML = totalScore);
+        }, 1100);
+      }
+    } else {
+      out = false;
+      header.innerHTML = "Current Score";
+      gameover.style.visibility = "hidden";
+      advancePlayer();
+      for (let i = 0; i < 6; i++) {
+        sides[i].style.backgroundColor = "#000000";
+        sides[i].style.backgroundImage =
+          "linear-gradient(147deg, #000000 0%, #2c3e50 74%)";
+      }
+    }
+
+    if (randomAngle === 1) {
+      out = true;
+      firstGo = 1;
+    }
   }
 });
