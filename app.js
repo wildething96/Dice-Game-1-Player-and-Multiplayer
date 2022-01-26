42594a02024e1321e0533712a3baecd55170d809
wildething96 <bwilde96@yahoo.co.uk>
2022-01-26 02:15:49 +0000

    Added draw functionality

--- app.js
+++ app.js
@@ -30,12 +30,15 @@ let highestScorer;
 let justClicked = false;
 let timeOut = 0;
 
-const advancePlayer = () => {
-  highestScore < totalScore
-    ? ((highestScore = totalScore), (highestScorer = currentPlayer))
-    : 0;
+const advancePlayer = (randomAngle) => {
+  if (randomAngle !== 1) {
+    highestScore < totalScore
+      ? ((highestScore = totalScore), (highestScorer = currentPlayer))
+      : 0;
+  }
   playerScores[currentPlayer - 1].innerHTML = totalScore;
   totalScore = 0;
+  firstGo = 1;
   currentPlayer++;
   currentScore.innerHTML = 0;
   playerNumber.innerHTML = currentPlayer;
@@ -53,10 +56,11 @@ const advancePlayer = () => {
 
 const restartGame = () => {
   totalScore = 0;
+  firstGo = 1;
   header.innerHTML = "Current Score";
   currentScore.innerHTML = totalScore;
   highestScore = 0;
-  highestScore = "";
+  highestScorer = 0;
   for (let i = 0; i < players; i++) {
     playerScores[i].innerHTML = "";
   }
@@ -70,19 +74,13 @@ const restartGame = () => {
   playerBox[0].style.opacity = "1.0";
 };
 
-holdButton.addEventListener("click", () => {
-  if (gameEnd) {
-    restartGame();
-  } else if (totalScore !== 0) {
-    advancePlayer();
-  }
-});
-
 const checkGameEnd = () => {
   if (currentPlayer > players - 1) {
     holdButton.innerHTML = "Restart Game?";
     playerNumber.innerHTML = currentPlayer - 1;
-    header.innerHTML = `The Winner is Player ${highestScorer}`;
+    highestScore !== 0
+      ? (header.innerHTML = `The Winner is Player ${highestScorer}!`)
+      : (header.innerHTML = "It was a draw!");
     gameEnd = true;
   }
 };
@@ -91,16 +89,11 @@ console.log(header);
 
 for (let i = 1; i < players; i++) {
   players / 2 >= i
-    ? ((leftSide.innerHTML += user.innerHTML),
-      console.log(0),
-      count > 3 ? (count = 0) : 0)
-    : ((rightSide.innerHTML += user.innerHTML), console.log(1));
-
+    ? ((leftSide.innerHTML += user.innerHTML), count > 3 ? (count = 0) : 0)
+    : (rightSide.innerHTML += user.innerHTML);
   playerBar[i].innerHTML = i + 1;
   count++;
 }
-
-console.log(playerBar);
 
 const addScore = (randomAngle) => {
   totalScore += randomAngle;
@@ -120,84 +113,92 @@ let angleArray = [
   [-133, -360, 53],
 ];
 
-console.log("1." + sides);
-
 playerBox[0].style.border = "2px solid yellow";
 playerBox[0].style.boxShadow = "0 0 10px 2px yellow";
 playerBox[0].style.opacity = "1.0";
 
-cube.addEventListener("click", (e) => {
-  
-  if(!justClicked){
-    
-  justClicked = true;
-  if (!gameEnd) {
-    /* ANIMATION */
-
-    let randomAngle = Math.floor(Math.random() * 6) + 1 + firstGo;
-    if (randomAngle === 7) {
-      randomAngle -= 1;
-    }
-
-    console.log(randomAngle);
-
-    if (randomAngle === 1) {
-      setTimeout(() => {
-        for (let i = 0; i < 6; i++) {
-          sides[i].style.transition = "1s ease";
-          sides[i].style.background =
-            "rgba(0, 0, 0, 0) linear-gradient(to right, rgb(235, 51, 73), rgb(244, 92, 67)) repeat scroll 0% 0%";
-        }
-        gameover.style.visibility = "visible";
-        header.innerHTML = "Game Over";
-        highestScore < totalScore
-          ? ((highestScore = totalScore), (highestScorer = currentPlayer))
-          : 0;
-        totalScore = 0;
-      }, 900);
-    }
-
-    if (out === false) {
-      firstGo = 0;
-      cube.style.animation = "animate 1s linear";
-
-      cube.style.transform =
-        "rotateX(" +
-        angleArray[randomAngle][0] +
-        "deg) rotateY(" +
-        angleArray[randomAngle][1] +
-        "deg) rotateZ(" +
-        angleArray[randomAngle][2] +
-        "deg)";
-      cube.style.transition = "1s linear";
-
-      if (randomAngle !== 1) {
-        cube.addEventListener("animationend", (e) => {
-          cube.style.animation = "";
-        });
-        setTimeout(() => {
-          addScore(randomAngle), (currentScore.innerHTML = totalScore);
-        }, 1100);
-      }
-    } else {
-      out = false;
-      header.innerHTML = "Current Score";
-      gameover.style.visibility = "hidden";
-      advancePlayer();
-      for (let i = 0; i < 6; i++) {
-        sides[i].style.backgroundColor = "#000000";
-        sides[i].style.backgroundImage =
-          "linear-gradient(147deg, #000000 0%, #2c3e50 74%)";
-      }
-    }
-
-    if (randomAngle === 1) {
-      out = true;
-      firstGo = 1;
-    }
+holdButton.addEventListener("click", () => {
+  if (gameEnd) {
+    restartGame();
+  } else if (totalScore !== 0) {
+    advancePlayer(0);
   }
-  setTimeout(() => {
-    justClicked = false;
-  }, 1100)
-}
+});
+
+cube.addEventListener("click", (e) => {
+  console.log(highestScore);
+  console.log(highestScorer);
+  if (!justClicked) {
+    justClicked = true;
+    if (!gameEnd) {
+      /* ANIMATION */
+
+      let randomAngle = Math.floor(Math.random() * 6) + 1 + firstGo;
+      if (randomAngle === 7) {
+        randomAngle -= 1;
+      }
+
+      console.log(randomAngle);
+
+      if (randomAngle === 1) {
+        setTimeout(() => {
+          for (let i = 0; i < 6; i++) {
+            sides[i].style.transition = "1s ease";
+            sides[i].style.background =
+              "rgba(0, 0, 0, 0) linear-gradient(to right, rgb(235, 51, 73), rgb(244, 92, 67)) repeat scroll 0% 0%";
+          }
+          gameover.style.visibility = "visible";
+          header.innerHTML = "Game Over";
+          if (randomAngle !== 1) {
+            highestScore < totalScore
+              ? ((highestScore = totalScore), (highestScorer = currentPlayer))
+              : 0;
+          }
+          totalScore = 0;
+        }, 900);
+      }
+
+      if (out === false) {
+        firstGo = 0;
+        cube.style.animation = "animate 1s linear";
+
+        cube.style.transform =
+          "rotateX(" +
+          angleArray[randomAngle][0] +
+          "deg) rotateY(" +
+          angleArray[randomAngle][1] +
+          "deg) rotateZ(" +
+          angleArray[randomAngle][2] +
+          "deg)";
+        cube.style.transition = "1s linear";
+
+        if (randomAngle !== 1) {
+          cube.addEventListener("animationend", (e) => {
+            cube.style.animation = "";
+          });
+          setTimeout(() => {
+            addScore(randomAngle), (currentScore.innerHTML = totalScore);
+          }, 1100);
+        }
+      } else {
+        out = false;
+        header.innerHTML = "Current Score";
+        gameover.style.visibility = "hidden";
+        advancePlayer(randomAngle);
+        for (let i = 0; i < 6; i++) {
+          sides[i].style.backgroundColor = "#000000";
+          sides[i].style.backgroundImage =
+            "linear-gradient(147deg, #000000 0%, #2c3e50 74%)";
+        }
+      }
+
+      if (randomAngle === 1) {
+        out = true;
+        firstGo = 1;
+      }
+    }
+    setTimeout(() => {
+      justClicked = false;
+    }, 1100);
+  }
 });
